19caa7fbf86cc21addd8282c430c6f2cfbeef3b4
EdwardLeeMacau <b05901119@ntu.edu.tw>
2023-05-29 14:00:49 +0800

    Bypass storing string to TensorBoard

--- scripts/image_optimization.py
+++ scripts/image_optimization.py
@@ -634,7 +634,7 @@ class RunTurbo():
         # store hyperparameters
         self.tracker.writer.add_hparams(hparams, {
             'score': self.args.Y.max().item(),
-            'best_prompt': self.args.P[self.args.Y.argmax()],
+            # 'best_prompt': self.args.P[self.args.Y.argmax()],
         })
 
         # write X, Y, P, to file
